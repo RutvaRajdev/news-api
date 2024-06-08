59e3eb45701b8caa67e0efb4a298f6fa1c8f2f48
Rutva Rajdev <rutva.rajdev@gmail.com>
2024-06-08 18:29:21 -0500

    Adding endpoint to search titles containing a specific keyword

--- src/main/java/com/fhf/it/news/api/controller/CustomServiceController.java
+++ src/main/java/com/fhf/it/news/api/controller/CustomServiceController.java
@@ -2,14 +2,14 @@ package com.fhf.it.news.api.controller;
 
 import com.fhf.it.news.api.model.EverythingResponse;
 import com.fhf.it.news.api.service.PublicNewsAPIService;
+import com.fhf.it.news.api.service.impl.PublicNewsAPIServiceImpl;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/service")
@@ -18,10 +18,24 @@ public class CustomServiceController {
     @Autowired
     private PublicNewsAPIService publicNewsAPIService;
 
-    @GetMapping(value = "/getByTitle/{title}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<EverythingResponse> getAllArticles(@PathVariable String title) {
+    private static final String TITLE = "title";
 
-        EverythingResponse allArticles = publicNewsAPIService.getAllArticles(title, "title", null, null, null, null, null, null, null, null, null);
+    private static final Logger LOGGER = LogManager.getLogger(CustomServiceController.class);
+
+    @GetMapping(value = "/getByTitle/{title}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<EverythingResponse> getByTitle(@PathVariable String title) {
+        LOGGER.warn("Searching for articles with title \"" + title + "\"");
+
+        EverythingResponse allArticles = publicNewsAPIService.getAllArticles(title, TITLE, null, null, null, null, null, null, null, 100, 1);
+        ResponseEntity<EverythingResponse> response = new ResponseEntity<>(allArticles, HttpStatus.OK);
+        return response;
+    }
+
+    @GetMapping(value = "/searchByKeywordInTitle", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<EverythingResponse> searchByKeywordInTitle(@RequestParam String keyword) {
+        LOGGER.warn("Searching for articles containing \"" + keyword + "\" in the title");
+
+        EverythingResponse allArticles = publicNewsAPIService.getAllArticles("+"+keyword, TITLE, null, null, null, null, null, null, null, 100, 1);
         ResponseEntity<EverythingResponse> response = new ResponseEntity<>(allArticles, HttpStatus.OK);
         return response;
     }
